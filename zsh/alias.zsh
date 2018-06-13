677f757e2ad528deba463b99da4c80fbe8ae0996
walsvid <walsvid@gmail.com>
2018-06-13 22:01:44 +0800

    walsvid - Add alais for remote server

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,4 +4,7 @@ alias abrew="ALL_PROXY=socks5://127.0.0.1:1086 brew"
 alias gflp="git push origin develop && git push origin master && git push --tags"
 alias setproxy="export http_proxy=http://127.0.0.1:1087; export https_proxy=https://127.0.0.1:1087; echo 'HTTP Proxy on';"
 alias unsetproxy="unset http_proxy; unset https_proxy; echo 'HTTP Proxy off';"
-
+alias rjupyter="jupyter notebook --no-browser --port=$RJUPYTER_PORT"
+alias rtensorboard="tensorboard --port=$RTENSORBOARD_PORT"
+alias cona="conda activate"
+alias cond="conda deactivate"
